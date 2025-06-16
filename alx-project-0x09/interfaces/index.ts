635eca785ff0d577205170c209e8de510ca7fb37
kaberege <kabgnestor@gmail.com>
2025-06-16 19:38:17 +0200

    added Image based on user prompt

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,9 +1,4 @@
-import * as React from "react"
 import { ReactNode } from "react";
-
-export interface ReactComponentProps {
-    children: React.ReactNode
-}
 
 export interface ReactComponentProps {
   children: ReactNode
@@ -16,6 +11,10 @@ export interface GeneratedImageProps {
   width?: string
   height?: string
   action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+  prompt: string;
 }
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
